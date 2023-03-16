b73ec73518cb8c6b1e3bbf9bd6c7e719197aac87
hwk42 <hwk42@outlook.com>
2023-03-16 19:35:38 +0800

    use hostnetwork

--- build-push-image-deploy.sh
+++ build-push-image-deploy.sh
@@ -1,5 +1,4 @@
 IMG=10.252.39.13:5000/aiscene/zeusapp-controller:v1
 docker build --build-arg https_proxy=10.1.180.122:20171 -t ${IMG} .
 docker push ${IMG}
-kubectl -nzeusapp-system delete deploy zeusapp-controller-manager
-kubectl apply -f config/default
+kubectl -nzeusapp-system get po |grep zeusapp-controller-manager | awk '{print $1}' | xargs kubectl -nzeusapp-system delete po 
