43790a0177069b03826d8619d849d446e2c11dc3
hwk42 <hwk42@outlook.com>
2023-03-16 10:37:28 +0800

    add pod ip env ref

--- build-push-image-deploy.sh
+++ build-push-image-deploy.sh
@@ -1,6 +1,3 @@
 IMG=10.252.39.13:5000/aiscene/zeusapp-controller:v1
 make docker-build docker-push IMG=${IMG}
-LOCALBIN ?= $(shell pwd)/bin
-KUSTOMIZE ?= $(LOCALBIN)/kustomize
-cd config/manager && $(KUSTOMIZE) edit set image controller=${IMG}
-cd - &&	$(KUSTOMIZE) build config/default | kubectl apply -f -
+make update
